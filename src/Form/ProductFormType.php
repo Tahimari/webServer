2c3a9e3c4a58d7a21bf8e532bfa919477d9d4479
Tahimari <kamilm215@gmail.com>
2019-04-26 16:15:32 +0200

    up

--- src/Form/ProductFormType.php
+++ src/Form/ProductFormType.php
@@ -6,17 +6,32 @@ use App\Entity\Item;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ProductFormType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('price')
+            ->add('title', null, ['attr' => ['rows' => 1]])
+            ->add('description', null, ['attr' => ['rows' => 5]])
+            ->add('price', MoneyType::class, [
+                'currency' => 'PLN'
+            ])
             ->add('image_url')
-            ->add('category')
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    'SHOES'    => 'SHOES',
+                    'JACKET'   => 'JACKET',
+                    'TROUSERS' => 'TROUSERS',
+                    'TSHIRT'   => 'TSHIRT',
+                    'SHIRTS'   => 'SHIRTS',
+                    'DELIVERY' => 'DELIVERY'
+                ]
+            ])
+            ->add('isVisible')
         ;
     }
 
